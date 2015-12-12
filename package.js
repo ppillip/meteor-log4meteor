e3d0479c01fdc1237c22fa937e444c688c24d277
ppillip <ppillip@gmail.com>
2015-12-12 21:39:35 +0900

    summary 수정
    summary 수정

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'ppillip:log4meteor',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'send your console.log to server',
+  summary: 'send your console.log result to server',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/ppillip/meteor-log4meteor.git',
   // By default, Meteor will default to using README.md for documentation.
